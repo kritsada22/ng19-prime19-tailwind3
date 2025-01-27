048278c1e4c8b1df7d752ad4c3ec1fdf4df98e1f
kritsada22 <kritsada601010@gmail.com>
2025-01-27 16:15:44 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Button, ButtonModule} from "primeng/button";
+import {InputText, InputTextModule} from "primeng/inputtext";
+import {Select, SelectModule} from "primeng/select";
+import {FormsModule} from "@angular/forms";
+import {Textarea, TextareaModule} from "primeng/textarea";
+import {DatePickerModule} from "primeng/datepicker";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+  imports: [RouterOutlet, ButtonModule, InputTextModule, SelectModule, FormsModule, TextareaModule, DatePickerModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'test-ng19';
+  countries: any[] | undefined = [
+{
+  name: 'name',
+},{
+  name: 'name2'
+}]
+  selectedCountry: any;
 }
